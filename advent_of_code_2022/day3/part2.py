36dd2f81555eef6636e803870b0406c4894fd347
Gyanbu <gyanbu@protonmail.com>
2022-12-03 15:55:00 +0100

    part2: first code

---
+++ advent_of_code_2022/day3/part2.py
@@ -0,0 +1,27 @@
+import string
+
+with open('data.txt', 'rt') as f:
+    data = [line.strip() for line in f.readlines()]
+    # data = f.readlines()
+print(data)
+
+
+alphabet = string.ascii_letters
+bags = []
+for bag in data:
+    buf = [[], []]
+    for letter in bag[:len(bag) // 2]:
+        buf[0].append(alphabet.index(letter))
+    for letter in bag[len(bag) // 2:]:
+        buf[1].append(alphabet.index(letter))
+    bags.append(buf)
+
+
+answer = 0
+for elf_group in range(len(bags) // 3):
+    elf_group = bags[elf_group * 3:elf_group * 3 + 3]
+    for item in elf_group[0]:
+        if item in elf_group[1] and item in elf_group[2]:
+            answer += item + 1
+            continue
+print(answer)
