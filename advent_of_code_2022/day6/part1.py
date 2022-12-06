860ee5fe6607f77197585f31943cc284046b3e67
Gyanbu <gyanbu@protonmail.com>
2022-12-06 19:03:01 +0100

    part 1 & 2 done!

--- advent_of_code_2022/day6/part1.py
+++ advent_of_code_2022/day6/part1.py
@@ -1,4 +1,22 @@
 with open('data.txt', 'rt') as f:
-    data = [line.strip() for line in f.readlines()]
+    data = f.readline().strip()
     # data = f.readlines()
 print(data)
+# data = 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'  # 11
+
+
+def is_marker(string):
+    for letter in string:
+        if string.count(letter) > 1:
+            return False
+    return True
+
+
+seq_len = 4
+for i in range((len(data) - seq_len + 1)):
+    buf = data[i:i+seq_len]
+
+    if is_marker(buf):
+        print(i + seq_len)
+        break
+
---
+++ advent_of_code_2022/day6/part2.py
@@ -0,0 +1,22 @@
+with open('data.txt', 'rt') as f:
+    data = f.readline().strip()
+    # data = f.readlines()
+print(data)
+# data = 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg'  # 29
+
+
+def is_marker(string):
+    for letter in string:
+        if string.count(letter) > 1:
+            return False
+    return True
+
+
+seq_len = 14
+for i in range((len(data) - seq_len + 1)):
+    buf = data[i:i+seq_len]
+
+    if is_marker(buf):
+        print(i + seq_len)
+        break
+
