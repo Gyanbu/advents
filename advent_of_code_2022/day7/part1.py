46924e7e70ba299d6d8b360c2298da7a89f844ca
Gyanbu <gyanbu@protonmail.com>
2022-12-07 14:29:51 +0100

    part 1: creating directory structure from data (probably works too!)

--- advent_of_code_2022/day7/part1.py
+++ advent_of_code_2022/day7/part1.py
@@ -2,3 +2,49 @@ with open('data.txt', 'rt') as f:
     data = [line.strip() for line in f.readlines()]
     # data = f.readlines()
 print(data)
+
+
+min_file_size = 100000
+
+
+def add_directory_to_tree(tree, path, name):
+    for d, dir in enumerate(path):
+        if d == 0:
+            buf = tree[dir]
+        else:
+            buf = buf[dir]
+
+    if not buf.get(name, False):
+        buf[name] = {}
+
+
+def add_file_to_directory(tree, path, name, size):
+    for d, dir in enumerate(path):
+        if d == 0:
+            buf = tree[dir]
+        else:
+            buf = buf[dir]
+
+    buf[name] = size
+
+
+directories_tree = {'/': {}}
+path = []
+for line in data:
+    buf = line.split(' ')
+    if buf[0] == '$':
+        if buf[1] == 'cd':
+            if buf[2] == '':
+                path = []
+            elif buf[2] == '..':
+                path.pop()
+            else:
+                path.append(buf[2])
+
+    else:  # ls
+        if buf[0] == 'dir':
+            # if not directories_tree.get(buf[0], False):
+            add_directory_to_tree(directories_tree, path, buf[1])
+        else:  # file
+            add_file_to_directory(directories_tree, path, buf[1], int(buf[0]))
+print(directories_tree)
