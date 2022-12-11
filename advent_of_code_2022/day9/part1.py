6732d96df80b5cb4bbdd2184ae356bf9dd83a9f0
Gyanbu <gyanbu@protonmail.com>
2022-12-11 17:31:52 +0100

    part 1: Code for moving head

--- advent_of_code_2022/day9/part1.py
+++ advent_of_code_2022/day9/part1.py
@@ -6,4 +6,30 @@
 with open('test_data.txt', 'rt') as f:
     data = [line.strip() for line in f.readlines()]
     # data = f.readlines()
+# print(data)
+
+
+def move_head(position, direction):
+    if direction == 'L':
+        position[0] -= 1
+    elif direction == 'R':
+        position[0] += 1
+    elif direction == 'D':
+        position[1] -= 1
+    elif direction == 'U':
+        position[1] += 1
+
+
+for i, instruction in enumerate(data):
+    data[i] = instruction.split(' ')
+    data[i][1] = int(data[i][1])
 print(data)
+
+head = [0, 0]
+tail = [0, 0]
+
+for instruction in data:
+    for _ in range(instruction[1]):
+        move_head(head, instruction[0])
+
+        print(head)
