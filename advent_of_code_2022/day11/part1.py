e280b2e5abe7b8916748aa5223e787b910193f08
Gyanbu <gyanbu@protonmail.com>
2022-12-12 20:49:53 +0100

    Part 1: Parsed data

--- advent_of_code_2022/day11/part1.py
+++ advent_of_code_2022/day11/part1.py
@@ -8,3 +8,38 @@ with open('test_data.txt', 'rt') as f:
     # data = f.readlines()
 print(data)
 
+
+m = 0
+monkeys = []
+for line in data:
+    if 'Monkey' in line:
+        m = int(line[7:-1])
+        monkeys.append({})
+
+    if 'Starting items:' in line:
+        monkeys[m]['Items'] = list(map(int, line[16:].split(', ')))
+
+    if 'Operation:' in line:
+        if '*' in line:
+            try:
+                monkeys[m]['Operation'] = ('*', int(line.split('* ')[1]))
+            except ValueError:
+                monkeys[m]['Operation'] = ('*', 'old')
+        elif '+' in line:
+            try:
+                monkeys[m]['Operation'] = ('+', int(line.split('+ ')[1]))
+            except ValueError:
+                monkeys[m]['Operation'] = ('+', 'old')
+        else:
+            raise 'Not supported math'
+
+    if 'Test' in line:
+        monkeys[m]['Test'] = int(line[19:])
+
+    if 'true' in line:
+        monkeys[m]['True'] = int(line[25:])
+
+    if 'false' in line:
+        monkeys[m]['False'] = int(line[26:])
+print(monkeys)
+
