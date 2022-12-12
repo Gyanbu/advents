2cc7f28241e44603ca4895df27824f9a7625f20d
Gyanbu <gyanbu@protonmail.com>
2022-12-12 21:25:39 +0100

    Part 2: Probably a solution

---
+++ advent_of_code_2022/day11/part2.py
@@ -0,0 +1,80 @@
+import time
+
+with open('data.txt', 'rt') as f:
+    data = [line.strip() for line in f.readlines()]
+    # data = f.readlines()
+print(data)
+
+# with open('test_data.txt', 'rt') as f:
+#     data = [line.strip() for line in f.readlines()]
+#     # data = f.readlines()
+# print(data)
+
+
+m = 0
+monkeys = []
+for line in data:
+    if 'Monkey' in line:
+        m = int(line[7:-1])
+        monkeys.append({})
+
+    if 'Starting items:' in line:
+        monkeys[m]['Items'] = list(map(int, line[16:].split(', ')))
+
+    if 'Operation:' in line:
+        if '*' in line:
+            try:
+                monkeys[m]['Operation'] = ('*', int(line.split('* ')[1]))
+            except ValueError:
+                monkeys[m]['Operation'] = ('*', 'old')
+        elif '+' in line:
+            try:
+                monkeys[m]['Operation'] = ('+', int(line.split('+ ')[1]))
+            except ValueError:
+                monkeys[m]['Operation'] = ('+', 'old')
+        else:
+            raise 'Not supported math'
+
+    if 'Test' in line:
+        monkeys[m]['Test'] = int(line[19:])
+
+    if 'true' in line:
+        monkeys[m]['True'] = int(line[25:])
+
+    if 'false' in line:
+        monkeys[m]['False'] = int(line[26:])
+print(monkeys)
+
+t = time.time()
+monkey_business = [0] * len(monkeys)
+for _ in range(10000):
+    print(f'Round: {_}')
+    for m, monkey in enumerate(monkeys):
+        for _ in range(len(monkey['Items'])):
+            monkey_business[m] += 1
+
+            item = monkey['Items'].pop(0)
+            if monkey['Operation'][0] == '*':
+                if monkey['Operation'][1] == 'old':
+                    item *= item
+                else:
+                    item *= monkey['Operation'][1]
+            elif monkey['Operation'][0] == '+':
+                if monkey['Operation'][1] == 'old':
+                    item += item
+                else:
+                    item += monkey['Operation'][1]
+
+            # item //= 3
+
+            if item % monkey['Test'] == 0:
+                monkeys[monkey['True']]['Items'].append(item)
+            else:
+                monkeys[monkey['False']]['Items'].append(item)
+
+monkey_business = sorted(monkey_business, reverse=True)
+print(monkey_business[0] * monkey_business[1])
+
+with open('output.txt', 'wt') as f:
+    f.write(f'Took {round(time.time() - t)} seconds, answer:\n')
+    f.write(str(monkey_business[0] * monkey_business[1]))
