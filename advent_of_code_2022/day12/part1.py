0f3716bcbb36e133b08f33a48e813b708e3b9dbd
Gyanbu <gyanbu@protonmail.com>
2022-12-25 17:11:40 +0100

    part 1: Done!

--- advent_of_code_2022/day12/part1.py
+++ advent_of_code_2022/day12/part1.py
@@ -1,14 +1,15 @@
 import string
+import sys
 
-# with open('data.txt', 'rt') as f:
-#     data = [line.strip() for line in f.readlines()]
-#     # data = f.readlines()
-# print(data)
-
-with open('test_data.txt', 'rt') as f:
+with open('data.txt', 'rt') as f:
     data = [line.strip() for line in f.readlines()]
     # data = f.readlines()
 print(data)
+
+# with open('test_data.txt', 'rt') as f:
+#     data = [line.strip() for line in f.readlines()]
+#     # data = f.readlines()
+# print(data)
 
 
 alphabet = string.ascii_letters
@@ -38,16 +39,17 @@ def check_neighbors(pos, steps_map):
     steps = steps_map[pos_y][pos_x]
     height = height_map[pos_y][pos_x]
     for x, y in (pos_x - 1, pos_y), (pos_x + 1, pos_y), (pos_x, pos_y - 1), (pos_x, pos_y + 1):
-        if x < 0 or y < 0 or x > len(steps_map[0]) - 1 or y > len(steps_map) - 1 or steps_map[y][x] != float('inf'):
+        if x < 0 or y < 0 or x > len(steps_map[0]) - 1 or y > len(steps_map) - 1: # or steps_map[y][x] != float('inf'):
             continue
 
-        if height + 1 >= height_map[y][x]:
+        if height + 1 >= height_map[y][x] and steps + 1 < steps_map[y][x]:
             steps_map[y][x] = steps + 1
             check_neighbors((x, y), steps_map)
         # print(f'{x}, {y}')
 
 
+sys.setrecursionlimit(10000000)
 check_neighbors(start, steps_map)
 print(steps_map[end[1]][end[0]])
-for line in steps_map:
-    print(line)
+# for line in steps_map:
+#     print(line)
