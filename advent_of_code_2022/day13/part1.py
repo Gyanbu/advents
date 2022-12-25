affb68461a7939628de7f3bb0876f813411cb101
Gyanbu <gyanbu@protonmail.com>
2022-12-25 22:37:24 +0100

    day 13: init

---
+++ advent_of_code_2022/day13/part1.py
@@ -0,0 +1,9 @@
+# with open('data.txt', 'rt') as f:
+#     data = [line.strip() for line in f.readlines()]
+#     # data = f.readlines()
+# print(data)
+
+with open('test_data.txt', 'rt') as f:
+    data = [line.strip() for line in f.readlines()]
+    # data = f.readlines()
+print(data)
