3dd289a32a461f7ec725770e99cf554cdbea489f
Gyanbu <gyanbu@protonmail.com>
2023-12-01 14:46:15 +0100

    Day 10: I

--- advent_of_code_2022/day12/part1.py
+++ advent_of_code_2022/day12/part1.py
@@ -9,9 +9,38 @@ from functools import cache
 with open('test_data.txt', 'rt') as f:
     data = [line.strip() for line in f.readlines()]
     # data = f.readlines()
-print(data)
+print(f'{data}')
 
 
 alphabet = string.ascii_letters
 print(alphabet)
 
+terrain = []
+for line in data:
+    terrain.append([alphabet.index(letter) for letter in line])
+print(terrain)
+
+for j, y in enumerate(terrain):
+    for i, x in enumerate(y):
+        if x == 44:
+            y[i] = alphabet.index('a')
+            start = (i, j)
+        elif x == 30:
+            y[i] = alphabet.index('z')
+            end = (i, j)
+print(terrain)
+print(f'{start = }\n{end = }')
+
+
+def path(start=None, end=None, terrain=None, pos=None, path=[], visited=[]):
+    if start is not None:
+        pos = start
+
+    if pos[0] != 0:
+        if terrain[pos[0] - 1, pos[1]] - terrain[pos[0], pos[1]] <= 1:
+            visited_copy = visited.copy()
+            visited_copy.append()
+            path()
+
+
+path(start=start, end=end, terrain=terrain)
---
+++ advent_of_code_2023/day1/part2.py
@@ -0,0 +1,35 @@
+with open('data.txt', 'rt') as f:
+# with open('test_data.txt', 'rt') as f:
+    data = [line.strip() for line in f.readlines()]
+# print(data[:4])
+
+nums = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+for l in range(len(data)):
+    buf = ''
+    for char in data[l]:
+        buf += char
+        for i in range(len(nums)):
+            while True:
+                if (j := buf.find(nums[i])) != -1:
+                    buf = buf[:j] + str(i) + buf[j + len(nums[i]):]
+                    continue
+                break
+    data[l] = buf
+
+result = 0
+for line in data:
+    first = None
+    last = None
+    for char in line:
+        if char.isdigit():
+            if first is None:
+                first = int(char)
+            else:
+                last = int(char)
+
+    if last is None:
+        last = first
+
+    result += first * 10 + last
+    print(first * 10 + last)
+print(result)
