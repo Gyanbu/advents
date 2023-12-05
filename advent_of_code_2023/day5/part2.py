263d437b43084301f3deadde144c2692121b127c
Gyanbu <gyanbu@protonmail.com>
2023-12-05 20:08:59 +0100

    crunching too slow :<

--- advent_of_code_2023/day5/part2.py
+++ advent_of_code_2023/day5/part2.py
@@ -1,5 +1,5 @@
-# with open('data.txt', 'rt') as f:
-with open('test_data.txt', 'rt') as f:
+with open('data.txt', 'rt') as f:
+# with open('test_data.txt', 'rt') as f:
     data = [line.strip() for line in f.readlines()]
 
 
@@ -7,6 +7,7 @@ seeds = [int(num) for num in data[0].split()[1:]]
 seed_ranges = []
 for i in range(len(seeds)//2):
     seed_ranges.append((seeds[i*2], seeds[i*2+1]))
+seed_ranges = sorted(seed_ranges, key=lambda x: x[0], reverse=True)
 print(seed_ranges)
 
 data = data[3:]
@@ -24,10 +25,13 @@ for line in data:
 maps.append(map_buf)
 maps = maps[::-1]
 
-seeds = [0]
-for seed in seeds:
+
+s = 0
+while True:
+    seed = s
+    s += 1
+    print(seed, end='<--')
     for map_instruction in maps:
-        print(seed, end='<-')
         for num in sorted(map_instruction.keys()):
             if seed < num:
                 break
@@ -35,16 +39,8 @@ for seed in seeds:
                 shift = num - map_instruction[num][0]
                 seed -= shift
                 break
-            else:
-                break
     print(seed)
-
-    print(seed, end='-->')
-    for map_instruction in maps:
-        for num in sorted(map_instruction.keys(), reverse=True):
-            if seed >= num and seed <= num + map_instruction[num][1] - 1:
-                # print(f'{seed}: {num} {map_instruction[num][0]} {map_instruction[num][1]}')
-                shift = map_instruction[num][0] - num
-                seed += shift
-                break
-    print(seed)
+    for index, length in seed_ranges:
+        if index <= seed < index + length:
+            print(seed)
+            exit()
